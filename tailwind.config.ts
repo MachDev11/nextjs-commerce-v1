1072e72233fa3bbf95dab09362e3c0351ff0d3f0
MachDev11 <seanrpittman@gmail.com>
2025-02-01 13:24:44 -0700

    UI like Medusa

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,5 +52,17 @@ const config: Config = {
     })
   ]
 };
+module.exports = {
+  presets: [require('@medusajs/ui-preset')],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
 export default config;
