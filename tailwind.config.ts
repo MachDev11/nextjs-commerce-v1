3fd193ed65b3cb2dcbe9c4e56c2ea5a7e3b3ee0f
MachDev11 <seanrpittman@gmail.com>
2025-02-01 13:45:04 -0700

    Medusa UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,56 +1,19 @@
-import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
+// tailwind.config.ts
+import { Config } from 'tailwindcss';
+import medusaUiPreset from '@medusajs/ui-preset';
 
 const config: Config = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  presets: [medusaUiPreset],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-geist-sans)']
-      },
-      keyframes: {
-        fadeIn: {
-          from: { opacity: '0' },
-          to: { opacity: '1' }
-        },
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' }
-        },
-        blink: {
-          '0%': { opacity: '0.2' },
-          '20%': { opacity: '1' },
-          '100%': { opacity: '0.2' }
-        }
-      },
-      animation: {
-        fadeIn: 'fadeIn .3s ease-in-out',
-        carousel: 'marquee 60s linear infinite',
-        blink: 'blink 1.4s both infinite'
-      }
-    }
+    extend: {},
   },
-  future: {
-    hoverOnlyWhenSupported: true
-  },
-  plugins: [
-    require('@tailwindcss/container-queries'),
-    require('@tailwindcss/typography'),
-    plugin(({ matchUtilities, theme }) => {
-      matchUtilities(
-        {
-          'animation-delay': (value) => {
-            return {
-              'animation-delay': value
-            };
-          }
-        },
-        {
-          values: theme('transitionDelay')
-        }
-      );
-    })
-  ]
+  plugins: [],
 };
 module.exports = {
   presets: [require('@medusajs/ui-preset')],
